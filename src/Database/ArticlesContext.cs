0fdbaa9943ec01acadc57fd70cf9ef1dcbdc14e9
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-10-07 16:26:50 +0100

    Project Creation

---
+++ src/Database/ArticlesContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Threenine;
+
+namespace Database.Articless;
+
+public class ArticlesContext : BaseContext<ArticlesContext>
+{
+    public ArticlesContext(DbContextOptions<ArticlesContext> options)
+        : base(options)
+    {
+    }
+  
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(DefaultSchema.Name);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
---
+++ src/Database/ArticlesContextFactory.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Database.Articless;
+
+internal class ArticlesContextFactory : IDesignTimeDbContextFactory<ArticlesContext>
+{
+    public ArticlesContext CreateDbContext(string[] args)
+    {
+        DbContextOptionsBuilder<ArticlesContext> dbContextOptionsBuilder =
+            new();
+
+        dbContextOptionsBuilder.UseNpgsql(ConnectionStringNames.LocalBuild);
+        return new ArticlesContext(dbContextOptionsBuilder.Options);
+    }
+}
