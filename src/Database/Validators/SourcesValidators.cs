6cdd4fe60bf0c52a60ddb8a38e741f47ec9ac82f
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-11-08 08:19:04 +0000

    Updated the Validation

--- src/Database/Validators/SourcesValidators.cs
+++ src/Database/Validators/SourcesValidators.cs
@@ -1,4 +1,6 @@
 using System.Data;
+using System.Text.RegularExpressions;
+using Common;
 using FluentValidation;
 using Geekiam.Data;
 
@@ -8,15 +10,17 @@ public class SourcesValidators : AbstractValidator<Sources>
 {
     public SourcesValidators()
     {
-        /*RuleFor(x => x.Name).NotEmpty();
+        RuleFor(x => x.Name).NotEmpty();
 
         RuleFor(x => x.Name).MaximumLength(255);
         RuleFor(x => x).Must(x => x.Name != x.Domain).WithMessage("The name of the source cannot be the same as the domain name").WithName("Name");
         
         RuleFor(x => x.Domain).NotEmpty();
-        RuleFor(x => x.Domain).Must(uri => Uri.TryCreate(uri, UriKind.Relative, out _)).When(x => !string.IsNullOrEmpty(x.Domain));
+        RuleFor(x => x.Domain).Matches(RegularExpressions.DomainName, RegexOptions.IgnoreCase)
+            .WithMessage("Not a valid domain name");
 
         RuleFor(x => x.FeedUrl).NotEmpty();
-        RuleFor(x => x.FeedUrl).Must(uri => Uri.TryCreate(uri, UriKind.Absolute, out _)).When(x => !string.IsNullOrEmpty(x.FeedUrl));*/
+        RuleFor(x => x.FeedUrl).Matches(RegularExpressions.RelativeUrlPath, RegexOptions.IgnoreCase)
+            .WithMessage("A relative url must be supplied");
     }
 }
